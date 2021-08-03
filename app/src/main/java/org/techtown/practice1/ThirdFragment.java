4d9b991039a6b1d09d18d00bbf023cf775689eac
armyants531 <giantmathbox@gmail.com>
2021-08-03 21:49:06 +0900

    Third

--- app/src/main/java/org/techtown/practice1/ThirdFragment.java
+++ app/src/main/java/org/techtown/practice1/ThirdFragment.java
@@ -1,11 +1,13 @@
 package org.techtown.practice1;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 
@@ -24,9 +27,9 @@ import java.util.Locale;
 public class ThirdFragment extends Fragment {
 
     EditText editText1, editText2;
-    Button calendarButton, addHomework;
+    Button calendarButton, addHomework, clockButton;
     CheckBox check1, check2, check3;
-    int checkBoxChecker = 0;
+    int checkBoxChecker = 0, alarmHour = 0, alarmMinute = 0;
 
     Calendar calendar = Calendar.getInstance();
     // default 값은 오늘 날짜로 설정
@@ -51,6 +54,7 @@ public class ThirdFragment extends Fragment {
         editText2 = rootView.findViewById(R.id.editText2);
         calendarButton = rootView.findViewById(R.id.calendarButton);
         addHomework = rootView.findViewById(R.id.addHomework);
+        clockButton = rootView.findViewById(R.id.clockButton);
 
         Date currentTime = Calendar.getInstance().getTime();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -61,6 +65,20 @@ public class ThirdFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 new DatePickerDialog(getContext(), setDate, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        clockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                            }
+                        }, alarmHour, alarmMinute, false);
+                timePickerDialog.show();
             }
         });
 
