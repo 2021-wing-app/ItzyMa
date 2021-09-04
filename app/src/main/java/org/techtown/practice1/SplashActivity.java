5a866fa91ae4d7373b42d64a674b77c101f5a2d8
inthree3 <42310616+inthree3@users.noreply.github.com>
2021-09-04 17:26:24 +0900

    splash_screen_done

--- app/src/main/java/org/techtown/practice1/SplashActivity.java
+++ app/src/main/java/org/techtown/practice1/SplashActivity.java
@@ -1,14 +1,28 @@
 package org.techtown.practice1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
 
+import java.util.logging.LogRecord;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        
+        handler.postDelayed(new Runnable(){
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                
+                finish();
+            }
+        }, 1000);
     }
 }
